fd7f8e81fe190af191aad4be9b46869692955f35
wqdeng <dengweiq@gmail.com>
2025-07-14 21:05:47 +0800

    feat: 第三章作业
    1.更新提示词

--- src/llm.py
+++ src/llm.py
@@ -1,25 +1,28 @@
 # src/llm.py
-
-import os
+import json
 from openai import OpenAI
 
 class LLM:
     def __init__(self):
         self.client = OpenAI()
+        # 加载提示词
+        with open('prompts/report_prompt.txt', 'r', encoding='utf8') as prompt_file:
+            self.system_prompt = prompt_file.read()
 
     def generate_daily_report(self, markdown_content, dry_run=False):
-        prompt = f"以下是项目的最新进展，根据功能合并同类项，形成一份文本格式的中文简报，至少包含：1）新增功能；2）主要改进；3）修复问题；:\n\n{markdown_content}"
+        messages=[
+            {"role": "system", "content": self.system_prompt},
+            {"role": "user", "content": markdown_content}
+        ]
         if dry_run:
             with open("daily_progress/prompt.txt", "w+") as f:
-                f.write(prompt)
+                f.write(json.dumps(messages, indent=4, ensure_ascii=False))
             return "DRY RUN"
 
         print("Before call GPT")
         response = self.client.chat.completions.create(
             model="gpt-3.5-turbo",
-            messages=[
-                {"role": "user", "content": prompt}
-            ]
+            messages=messages
         )
         print("After call GPT")
         print(response)
